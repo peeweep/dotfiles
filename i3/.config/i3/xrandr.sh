f0a3dff797f3676f155210e194a856a662e5455b
jinqiang zhang <peeweep@0x0.ee>
2024-09-23 11:58:44 +0000

    i3: fix adjust monitors

--- i3/.config/i3/xrandr.sh
+++ i3/.config/i3/xrandr.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-# primary dispaly on the left
-# secondary dispaly on the right and rotate left
+# primary 4K dispaly on the left
+# secondary 1080P dispaly on the right and rotate left
 adjust_monitors() {
   all_monitors=$(xrandr --query | grep ' connected' | awk '{print $1}')
   # must have 2 monitors
   if [[ $(echo "${all_monitors}" | wc -l) -ne 2 ]]; then
     return
   fi
-  left_monitor=$(xrandr --query | grep ' connected' | grep 2160 | awk '{print $1}')
-  right_monitor=$(xrandr --query | grep ' connected' | grep 1080 | awk '{print $1}')
-  if [[ "${left_monitor}" != "" && "${right_monitor}" != "" ]]; then
-    # init dp
-    xrandr --output "${right_monitor}" --auto
-    # init hdmi
-    xrandr --output "${left_monitor}" --auto
-    # hdmi left of dp
-    xrandr --output "${left_monitor}" --left-of "${right_monitor}"
-    # hdmi rotate left
-    xrandr --output "${right_monitor}" --rotate left
-    # dp primary
-    xrandr --output "${left_monitor}" --primary
+  monitor_4k=$(xrandr --query | grep ' connected' | grep 2160 | awk '{print $1}')
+  monitor_1080p=$(xrandr --query | grep ' connected' | grep 1080 | awk '{print $1}')
+  if [[ "${monitor_4k}" != "" && "${monitor_1080p}" != "" ]]; then
+    # init 4k
+    xrandr --output "${monitor_4k}" --auto
+    # init 1080p
+    xrandr --output "${monitor_1080p}" --auto
+    # 4k on the left
+    xrandr --output "${monitor_4k}" --left-of "${monitor_1080p}"
+    # 1080p rotate left
+    xrandr --output "${monitor_1080p}" --rotate left
+    # 4k primary
+    xrandr --output "${monitor_4k}" --primary
   fi
 
   i3 restart
